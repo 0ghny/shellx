bb39017af4a3e5bc5e42333d834d8da64d5da31a
0ghny <6640409+0ghny@users.noreply.github.com>
2022-09-04 14:37:47 +0200

    Make time calculation compatible with POSIX date implementations
    time::to_human_readable takes a timestamp and prints it using date command in a nice way.
    On OSX, it doesn't work properly due the implementation differences between linux and osx.
    To avoid making a dependency to brew with coreutils package, this commit fix it.

--- lib/core/time.sh
+++ lib/core/time.sh
@@ -9,5 +9,11 @@
 # echo Elapsed time: $(time::to_human_readable "$elapsed")
 time::to_human_readable() {
     local __input="$1"
-    eval "echo $(date -ud "@$__input" +'%H hr %M min %S sec')"
+
+    # Make it compatible with POSIX and Non-POSIX date implementations
+    if [[ "${OSTYPE}" == "linux"* ]]; then
+      eval "echo $(date -ud "@$__input" +'%H hr %M min %S sec')"
+    elif [[ "${OSTYPE}" == "darwin"* ]]; then
+      eval "echo $(date -u -r "$__input" +'%H hr %M min %S sec')"
+    fi
 }
