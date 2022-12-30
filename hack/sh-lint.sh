c9208b1d99e721a1cc2ec13c6143d5701b475542
0ghny <6640409+0ghny@users.noreply.github.com>
2022-12-30 22:32:30 +0100

    hack: adding scripts to run in local (if shellcheck installed) linting as BASH or POSIX sh

---
+++ hack/sh-lint.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -eu
+
+repo_root() {
+  git rev-parse --show-toplevel
+}
+
+needs_lint() {
+  [[ $1 == *.sh ]] && return 0
+  [[ $1 == */bash-completion/* ]] && return 0
+  [[ $(file -b --mime-type "$1") == text/x-shellscript ]] && return 0
+  return 1
+}
+
+while IFS= read -r -d $'' file; do
+  if needs_lint "$file"; then
+    shellcheck -W0 -s sh "$file" || continue
+  fi
+done < <(find "$(repo_root)" -type f  -not -path '*/\.git/*' -not -path '*/plugin-examples/*' -print0)
