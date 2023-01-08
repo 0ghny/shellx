c9a5562ed69ee219fbde58d85f6fba4201761e52
0ghny <6640409+0ghny@users.noreply.github.com>
2023-01-08 20:32:16 +0100

    feat(updater): added command to check version notes and logic to wether new version has to be checked or not

--- lib/shellx/version.sh
+++ lib/shellx/version.sh
@@ -4,7 +4,7 @@ shellx::version() {
   echo "$(git -C "${__shellx_homedir}" rev-parse --abbrev-ref HEAD)-$(git -C "${__shellx_homedir}" rev-parse --short HEAD)"
 }
 
-shellx:version:notes() {
+shellx::version::notes() {
   local count="${1:-3}"
   # shellcheck disable=SC2086
   git -C "${__shellx_homedir}" --no-pager \
