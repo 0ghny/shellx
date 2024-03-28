d6e1ec0ef778ae4cb513ba514733d3a9a4dcaf66
0ghny <6640409+0ghny@users.noreply.github.com>
2024-03-28 17:30:02 +0100

    improved shellx version info and shellx version notes output

--- lib/shellx/version.sh
+++ lib/shellx/version.sh
@@ -11,14 +11,25 @@ shellx::version() {
 
 shellx::version::notes() {
   local count="${1:-3}"
+
+  if [[ "${SHELLX_NO_COLOR:-false}" == "true" ]]; then
+    SHELLX_VERSION_NOTES_FORMAT="format: %<(20,trunc)%ar %h %<(12,trunc)%an %<(150,trunc)%s"
+  else
+    SHELLX_VERSION_NOTES_FORMAT="format:%C(green) %<(20,trunc)%ar %C(bold magenta)%h %C(bold green)%<(12,trunc)%an %C(bold yellow)%<(150,trunc)%s%C(reset)"
+  fi
+
   # shellcheck disable=SC2086
   git -C "${__shellx_homedir}" --no-pager \
-        log --pretty=format:"%h%x09%an%x09%ad%x09%s" -n${count}
+        log --no-merges --first-parent \
+            --pretty="${SHELLX_VERSION_NOTES_FORMAT}" \
+            -n${count} \
+            --invert-grep --grep="release" --grep="chore:" --grep="ci:"
 }
 
 shellx::version::info() {
   echo "Version Number: $(shellx::version)"
   echo "Release Notes (last 5):"
   shellx::version::notes 5
-  echo "for more information run shellx version notes <number of notes to show>"
+  echo ""
+  echo "  for more information run shellx version notes <number of notes to show>"
 }
